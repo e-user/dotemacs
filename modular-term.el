23d213ed0e262c1d8b6e1a9cf26a361afed6acc3
Alexander Kahl <alex@lshift.de>
2015-05-08 15:50:16 +0200

    Added C-esc

--- modular-term.el
+++ modular-term.el
@@ -67,7 +67,8 @@
             (cons "M-p" 'term-send-raw-meta)
             (cons "M-y" 'term-send-raw-meta)
             (cons "C-y" 'term-send-raw)
-            (cons "C-y" 'term-send-raw)))
+            (cons "C-y" 'term-send-raw)
+            (cons "<C-escape>" 'term-send-esc)))
 
 (global-set-key (kbd "<f5>") 'multi-term)
 (global-set-key (kbd "<C-next>") 'multi-term-next)
