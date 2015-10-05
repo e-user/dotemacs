a077c1263edafa04f070d794a30bf17c3c81a419
Alexander Kahl <alex@lshift.de>
2015-10-05 13:28:56 +0200

    Irony mode

---
+++ modular-irony.el
@@ -0,0 +1,34 @@
+;;; modular-irony.el --- Initialize irony            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Alexander Kahl
+
+;; Author: Alexander Kahl <e-user@fsfe.org>
+;; Keywords: emacs, autocompletion
+
+;;; Code:
+;;;###autoload
+(add-to-list 'modular-features 'modular-irony)
+
+(require 'modular-elpa)
+(require 'modular-company)
+(install 'irony 'company-irony)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+(provide 'modular-irony)
+;;; modular-irony.el ends here
