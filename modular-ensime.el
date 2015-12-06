e12d911778e91852f86f287651d5831e67ac5ea9
Alexander Kahl <alex@lshift.de>
2015-12-06 18:54:30 +0100

    Adding ensime

---
+++ modular-ensime.el
@@ -0,0 +1,22 @@
+;;; modular-ensime.el --- ENSIME Scala support       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Alexander Kahl
+
+;; Author: Alexander Kahl <e-user@fsfe.org>
+;; Keywords: languages
+
+;;; Code:
+;;;###autoload
+(add-to-list 'modular-features 'modular-ensime)
+
+(require 'modular-elpa)
+(install 'ensime)
+(require 'ensime)
+
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+(setq ensime-auto-generate-config t
+      ensime-sbt-command "sbt")
+
+(provide 'modular-ensime)
+;;; modular-ensime.el ends here
