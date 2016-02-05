4b2b1b0036738bb83dbad34763e1f7ead48fd482
Alexander Kahl <alex@lshift.de>
2016-02-05 12:41:30 +0100

    JS defaults

--- modules/modular-javascript.el
+++ modules/modular-javascript.el
@@ -1,9 +1,26 @@
-;;; modular-javascript.el --- Javascript init
+;;; modular-javascript.el --- Modular JavaScript module  -*- lexical-binding: t; -*-
 
-;; Copyright © 2015  Alexander Kahl
+;; Copyright (C) 2015-2016  Alexander Kahl
 
 ;; Author: Alexander Kahl <e-user@fsfe.org>
-;; Keywords: javascript, js
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Load js2
 
 ;;; Code:
 ;;;###autoload
@@ -13,7 +30,14 @@
 (require 'js2-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(setq js2-skip-preprocessor-directives t)
+(setq-default js2-skip-preprocessor-directives t
+              js2-auto-indent-p t
+              js2-basic-offset 2
+              js2-bounce-indent-p nil
+              js2-cleanup-whitespace t
+              js2-enter-indents-newline t
+              js2-indent-on-enter-key nil
+              js2-mirror-mode t)
 
 (provide 'modular-javascript)
 ;;; modular-javascript.el ends here
