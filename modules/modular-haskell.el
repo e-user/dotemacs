f49693969319c1f537e2eb5255eda6bfd959f863
Alexander Kahl <alex@lshift.de>
2016-02-05 14:12:35 +0100

    Updated header; made loading Company optional

--- modules/modular-haskell.el
+++ modules/modular-haskell.el
@@ -1,15 +1,30 @@
-;;; modular-haskell.el --- Haskell init
+;;; modular-haskell.el --- Modular Haskell module  -*- lexical-binding: t; -*-
 
-;; Copyright © 2015  Alexander Kahl
+;; Copyright © 2015-2016  Alexander Kahl
 
 ;; Author: Alexander Kahl <e-user@fsfe.org>
-;; Keywords: haskell
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Load Haskell
 
 ;;; Code:
 ;;;###autoload
 (add-to-list 'modular-features 'modular-haskell)
-
-(require 'modular-company)
 
 (install 'haskell-mode 'hindent 'ghc 'company-ghc 'shm)
 (add-hook 'haskell-mode-hook #'structured-haskell-mode)
@@ -40,7 +55,9 @@
   '(haskell-process-log t)
   '(haskell-process-type 'cabal-repl))
 
-(add-to-list 'company-backends 'company-ghc)
+
+(eval-after-load 'company
+  (add-to-list 'company-backends 'company-ghc))
 
 (let ((path (expand-file-name "~/.cabal/bin")))
   (setenv "PATH" (concat path ":" (getenv "PATH")))
