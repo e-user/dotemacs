9e4abefe12e240fd482c177ef06054eec88d8198
Alexander Kahl <alexander.kahl@oliverwyman.com>
2017-06-12 17:37:07 +0200

    Adding salt

---
+++ modules/modular-salt.el
@@ -0,0 +1,36 @@
+;;; modular-salt.el --- Modular Salt module          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Alexander Kahl
+
+;; Author: Alexander Kahl <ak@sodosopa.io>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Load Salt
+
+;;; Code:
+;;;###autoload
+(add-to-list 'modular-features 'modular-clojure)
+
+;;;###autoload
+(pin "melpa-stable" 'salt-mode)
+
+(install 'salt-mode)
+(require 'salt-mode)
+
+(provide 'modular-salt)
+;;; modular-salt.el ends here
