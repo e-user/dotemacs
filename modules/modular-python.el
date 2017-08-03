916ce74042a6670846fac3d38c3a868e12f0e253
Alexander Kahl <alexander.kahl@oliverwyman.com>
2017-08-03 11:47:07 +0200

    Added python with elpy

---
+++ modules/modular-python.el
@@ -0,0 +1,37 @@
+;;; modular-python.el --- Modular Python module      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Alexander Kahl
+
+;; Author: Alexander Kahl <ak@sodosopa.io>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Load Python
+
+;;; Code:
+;;;###autoload
+(add-to-list 'modular-features 'modular-python)
+
+;;;###autoload
+(pin "melpa-stable" 'elpy)
+
+(install 'elpy)
+
+(elpy-enable)
+
+(provide 'modular-python)
+;;; modular-python.el ends here
