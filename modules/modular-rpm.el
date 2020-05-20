126c842f3d20124570cce7ff69a7a03b2bb4c1df
Alexander Dorn <ad@sodosopa.io>
2020-05-20 14:08:36 +0200

    Add RPM support

---
+++ modules/modular-rpm.el
@@ -0,0 +1,36 @@
+;;; modular-rpm.el --- Modular RPM Spec mode         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Alexander Dorn
+
+;; Author: Alexander Dorn <ad@sodosopa.io>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Load RPM Spec support
+
+;;; Code:
+;;;###autoload
+(add-to-list 'modular-features 'modular-rpm)
+
+(install 'rpm-spec-mode)
+
+(autoload 'rpm-spec-mode "rpm-spec-mode.el" "RPM spec mode." t)
+(setq auto-mode-alist (append '(("\\.spec" . rpm-spec-mode))
+                              auto-mode-alist))
+
+(provide 'modular-rpm)
+;;; modular-rpm.el ends here
